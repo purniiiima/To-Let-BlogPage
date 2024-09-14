9e7d490a04ea72a765a8f1497cb40ac8f1d9d345
purniiiima <purniiiima@gmail.com>
2024-09-14 15:42:10 +0530

    Update deployment configuration

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from 'react-router-dom';
 
 import './index.css';
 import Layout from './Components/Layout';
@@ -19,34 +19,29 @@ import AuthorPosts from './pages/AuthorPosts';
 import Dashboard from './pages/Dashboard';
 import Logout from './pages/Logout';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout/>,
-    errorElement: <ErrorPage/>,
-    children: [
-      {index: true, element: <Home /> },
-      {path: "posts/:id", element: <PostDetail /> },
-      {path: "register", element: <Register /> },
-      {path: "login", element: <Login /> },
-      {path: "profile/:id", element: <UserProfile /> },
-      {path: "authors", element: <Authors /> },
-      {path: "create", element: <CreatePost /> },
-      {path: "posts/categories/:category", element: <CategoryPosts />},
-      {path: "posts/users/:id", elemrnt: <AuthorPosts/>},
-      {path: "myposts/:id", element: <Dashboard />},
-      {path: "posts/:id/edit", element: <EditPost />},
-      {path: "posts/:id/delete", element: <DeletePost />},
-      {path: "logout", element: <Logout /> },
-    ]
-  }
-])
+const routes = createRoutesFromElements(
+  <Route path="/" element={<Layout />}>
+    <Route index element={<Home />} />
+    <Route path="posts/:id" element={<PostDetail />} />
+    <Route path="register" element={<Register />} />
+    <Route path="login" element={<Login />} />
+    <Route path="profile/:id" element={<UserProfile />} />
+    <Route path="authors" element={<Authors />} />
+    <Route path="create" element={<CreatePost />} />
+    <Route path="posts/categories/:category" element={<CategoryPosts />} />
+    <Route path="posts/users/:id" element={<AuthorPosts />} />
+    <Route path="myposts/:id" element={<Dashboard />} />
+    <Route path="posts/:id/edit" element={<EditPost />} />
+    <Route path="posts/:id/delete" element={<DeletePost />} />
+    <Route path="logout" element={<Logout />} />
+  </Route>
+);
+
+const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-
